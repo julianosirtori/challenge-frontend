fef4964cc1b160e8ceb92fd0afbe832cca218be7
Juliano Sirtori <julianosirtori@gmail.com>
2024-01-06 11:07:58 -0300

    chore: add path alias to typescript config

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -1,6 +1,6 @@
-import Footer from "../../components/footer";
-import Header from "../../components/header";
-import Shelves from "../../components/shelves";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Shelves from "@/components/shelves";
 import {
   footerAllrightsReserved,
   helpLink,
