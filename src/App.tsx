d5b19d7a5e8aea103a9943996a04f35f36363122
Juliano Sirtori <julianosirtori@gmail.com>
2024-01-06 14:01:18 -0300

    feat: created global styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "@/lib/reactQuery";
 import { Router } from "@/routes";
 import { StylesManager } from "@/styles/StylesManager";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <QueryClientProvider>
       <StylesManager>
+        <GlobalStyle />
         <Router />
       </StylesManager>
     </QueryClientProvider>
