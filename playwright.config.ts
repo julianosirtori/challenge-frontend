bae0d5d1ca35f25986b402f05e0c642a29225e8b
Juliano Sirtori <julianosirtori@gmail.com>
2024-01-12 08:13:21 -0300

    feat: changed config on playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: 2,
-  workers: 1,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: "http://localhost:5173/",
+    baseURL: "http://localhost:4173/",
     trace: "on-first-retry",
   },
   projects: [
@@ -29,8 +29,8 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173/",
+    command: "npm run preview",
+    url: "http://localhost:4173/",
     reuseExistingServer: !process.env.CI,
   },
 });
